b4273fd4a2f9326e7015a84a4eea746ac899a093
EfraimGottlieb <efgo583208979@gmail.com>
2025-12-04 10:52:25 +0200

    askMultipleChoiceRiddle func added

--- utils/RiddleUtilityFunctions.js
+++ utils/RiddleUtilityFunctions.js
@@ -1,13 +1,30 @@
+import input from 'analiza-sync'
 import riddles from '../riddles/riddles.js'
+const enterAnswerPrint = () => {console.log('Enter the answer ')}
+
 const askMultipleChoiceRiddle = (riddleObj) => {
-  console.log('mc')
+  console.log('\noptions:\n')
+  let choiceIndex = 1
+  riddleObj.choices.forEach((choice) => {
+    console.log(`${choiceIndex}. ${choice}`)
+    choiceIndex ++
+  })
+  let answer = input('Enter the answer ')
+  while(answer -1 != riddleObj.correctAnswer) {
+    answer = input('Enter the answer ')
+  }
+  console.log('beautiful')
 }
 
 const askRegularRiddle = (riddleObj) => {
-  console.log('re')
+  
 }
 
 const askRiddle = (riddleObj) => {
+  console.log(riddleObj.name)
+  console.log(`riddle type: ${riddleObj.riddleType}`)
+  console.log(riddleObj.taskDescription)
+
   if(riddleObj.riddleType == 'multiple choice') {
     askMultipleChoiceRiddle(riddleObj)
   }
@@ -15,3 +32,5 @@ const askRiddle = (riddleObj) => {
     askRegularRiddle(riddleObj)
   }
 }
+// askRiddle(riddles[0])
+askRiddle(riddles[1])

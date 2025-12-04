bc5e21446af1f3f72611ee0fb41f9903351b2919
EfraimGottlieb <efgo583208979@gmail.com>
2025-12-04 10:28:38 +0200

    askRiddle func added

--- utils/RiddleUtilityFunctions.js
+++ utils/RiddleUtilityFunctions.js
@@ -1,1 +1,17 @@
 import riddles from '../riddles/riddles.js'
+const askMultipleChoiceRiddle = (riddleObj) => {
+  console.log('mc')
+}
+
+const askRegularRiddle = (riddleObj) => {
+  console.log('re')
+}
+
+const askRiddle = (riddleObj) => {
+  if(riddleObj.riddleType == 'multiple choice') {
+    askMultipleChoiceRiddle(riddleObj)
+  }
+  else if(riddleObj.riddleType == 'regular riddle') {
+    askRegularRiddle(riddleObj)
+  }
+}
